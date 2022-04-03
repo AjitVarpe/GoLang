31b09e48aff30ae22915c56a52d9a74f3ae5d00e
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-04-03 18:04:31 +0530

    added another files in git

---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// println("hello this is another yet new code of Go lang")
+	print("Lets Crate a simple game in Go lang :) \n")
+	var ticket = 5
+	const user = 15
+	var userName = "Ajit Varpe"
+
+	fmt.Printf("Tickets booked by %v are %v ", userName, ticket)
+}
