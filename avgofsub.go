23ae0f11469a601445c7c11fe9bce872a373a957
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-04-04 21:14:51 +0530

    added new codes

---
+++ avgofsub.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	print("Avarage of Three Subject is Given Below \n")
+	var phy = 60
+	var Bio = 70
+	var chem = 64
+	fmt.Printf("Avarage of Three Subject is %v", (Bio+chem+phy)/3)
+
+}
--- variable.go
+++ variable.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	// println("hello this is another yet new code of Go lang")
-	print("Lets Crate a simple game in Go lang :) \n")
+	print("Lets Create a simple game in Go lang :) \n")
 	var ticket = 5
 	const user = 15
 	var userName = "Ajit Varpe"
